9d6711a25564ca1d5afafd7eae9e8fe11fc9aefc
mdudzin <maciej.dudzin@gmail.com>
2019-09-09 18:24:24 +0200

    second commit

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 $(document).ready(function() {
 
-  var apiRoot = 'https://vast-cove-51102.herokuapp.com//v1/task/';
+  var apiRoot = 'https://frozen-beyond-13234.herokuapp.com/v1/task/';
   var datatableRowTemplate = $('[data-datatable-row-template]').children()[0];
   var tasksContainer = $('[data-tasks-container]');
 
